40a74a2deae83f3e56807d026a27471b74501ae6
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-05-04 18:15:56 +0800

    refactor(index.ts): move the config to the top of the file

--- index.ts
+++ index.ts
@@ -1,18 +1,21 @@
 import { fetchData, loadData } from './penguin-stats'
 import { solve } from './solver'
 
+// needed items. format: (itemID: count)
+const needs = { '30012': 100 }
+// had items. format: (itemID: count)
+const haves = { '30012': 50 }
+
 const init = new Date()
 fetchData().then(raw => {
   const fetched = new Date()
   console.log('lineark: fetched data in', fetched.getTime() - init.getTime(), 'ms')
 
   const data = loadData(raw)
-  let needs = new Map(Object.entries({ '30012': 100 })) as Map<string, number>
-  let haves = new Map(Object.entries({ '30012': 50 })) as Map<string, number>
   const loaded = new Date()
   console.log('lineark: loaded data in', loaded.getTime() - fetched.getTime(), 'ms')
 
-  solve(needs, haves, data)
+  solve(new Map(Object.entries(needs)), new Map(Object.entries(haves)), data)
   const solved = new Date()
   console.log('lineark: solved in', solved.getTime() - loaded.getTime(), 'ms')
 })
